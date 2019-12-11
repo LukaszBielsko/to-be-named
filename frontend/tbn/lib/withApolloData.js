a64830555994578bac6a297a3e15e914e9e787df
Lukasz Walczak <lukeabove@gmail.com>
2019-12-11 08:08:29 +0100

    apollo client set up

---
+++ frontend/tbn/lib/withApolloData.js
@@ -0,0 +1,24 @@
+import ApolloClient from 'apollo-boost';
+import withApollo from 'next-with-apollo';
+
+const endpoint = `http://localhost:4444`
+
+function createClient({headers}) { //headers for authentication 
+    return new ApolloClient({
+        uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+        request: operation => { // sort of express middleware
+            operation.setContext({
+                fetchOptions: {     
+                    credentials: 'include', // has to do with cookies and user being logged in or not 
+                },
+                headers,
+            });
+        },
+    })
+}
+
+export default withApollo(createClient) ; //withApollo is a HOC that will expose some props 
+
+
+
+// gql function for parsing your query string into a query document.
--- frontend/tbn/pages/_app.js
+++ frontend/tbn/pages/_app.js
@@ -1,19 +1,39 @@
-
 import App from 'next/app';
+import {ApolloProvider} from 'react-apollo';
+import withApolloData from '../lib/withApolloData';
 
 import Page from '../components/Page';
 
 class MyApp extends App {
-    render(){
 
-        const {Component} = this.props
+    /* TODO what does it to exactly?  */
+    /* this is next.js lifecycle method 
+       this will run first before render actually happens
+       and by returning sth, that sth will be available in the 
+       rendered component   
+    */
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {}
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx)
+        }
+        // this exposes the query to the user 
+        pageProps.query = ctx.query
+        return { pageProps }
+    }
+
+    render(){
+        /* TODO where this component came from? */
+        const {Component, apollo, pageProps} = this.props 
 
         return(
-                <Page>
-                    <Component/>
-                </Page>
+                <ApolloProvider client={apollo}>
+                    <Page>
+                        <Component {...pageProps}/>
+                    </Page>
+                </ApolloProvider>
         )
     }
 }
 
-export default MyApp ; 
+export default withApolloData(MyApp); // makes apollo prop available for MyApp component
