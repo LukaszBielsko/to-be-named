eb74bb730643d4b3cfec24736c54495124d3eeb7
Lukasz Walczak <lukeabove@gmail.com>
2019-12-23 08:37:52 +0100

    simple layout created

--- frontend/tbn/components/SingleItem.js
+++ frontend/tbn/components/SingleItem.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
 import { Query } from "react-apollo";
+import styled from 'styled-components';
+
+const StyledSingleItem = styled.div`
+  display: flex;
+  .image {
+
+  }
+  .info {
+
+  }
+`
 
 class SingleItem extends Component {
   render() {
-    return <p>single item</p>;
+    return (
+      <StyledSingleItem>
+        <div className="image">this is image</div>
+        <div className="info">
+          <p>title</p>
+          <p>place</p>
+          <p>desc</p>
+        </div>
+      </StyledSingleItem>
+    )
   }
 }
 export default SingleItem;
