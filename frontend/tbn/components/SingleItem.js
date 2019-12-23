8ee6e21bbb384a39cd4b05ec47af94a7133c7181
Lukasz Walczak <lukeabove@gmail.com>
2019-12-23 08:47:31 +0100

    graphQL query added to component

--- frontend/tbn/components/SingleItem.js
+++ frontend/tbn/components/SingleItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Query } from "react-apollo";
 import styled from 'styled-components';
+import gql from 'graphql-tag';
 
 const StyledSingleItem = styled.div`
   display: flex;
@@ -12,17 +13,33 @@ const StyledSingleItem = styled.div`
   }
 `
 
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY ($id: String!){
+    item(id: $id){
+      image
+      largeImage
+      title
+      place
+      description
+    }
+  }
+`
+
 class SingleItem extends Component {
   render() {
     return (
-      <StyledSingleItem>
-        <div className="image">this is image</div>
-        <div className="info">
-          <p>title</p>
-          <p>place</p>
-          <p>desc</p>
-        </div>
-      </StyledSingleItem>
+      <Query query={SINGLE_ITEM_QUERY}>
+        {() => (
+          <StyledSingleItem>
+            <div className="image">this is image</div>
+            <div className="info">
+              <p>title</p>
+              <p>place</p>
+              <p>desc</p>
+            </div>
+          </StyledSingleItem>
+        )}
+      </Query>
     )
   }
 }
