784ca6261c829bc8936ea58a1feb9756df765ac9
Lukasz Walczak <lukeabove@gmail.com>
2019-12-30 08:50:50 +0100

    jwt token verifaction added

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const createServer = require('./createServer')
 // const startDB = require('./mongo/connect')
 require('dotenv').config()
@@ -8,6 +9,21 @@ const server = createServer()
 // startDB()
 
 server.express.use(cookieParser())
+
+server.express.use((req, res, next) => {
+    const { jwtToken } = req.cookies
+    if (jwtToken) {
+        const { userId } = jwt.verify(jwtToken, process.env.APP_SECRET)
+        /* TODO why this is sticked to request
+            im handling a request here
+            im in the middle between request and response
+            so how this is possible to stick it on request?
+            request is being made by the client    
+        */
+        req.userId = userId
+    }
+    next()
+})
 
 server.start(
     {
