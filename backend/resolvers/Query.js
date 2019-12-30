82ffebd0be3f9b963cbb1dfc44d35d7df3027437
Lukasz Walczak <lukeabove@gmail.com>
2019-12-30 08:58:34 +0100

    graphql query and mongo query resolver added

--- backend/resolvers/Query.js
+++ backend/resolvers/Query.js
@@ -1,5 +1,6 @@
 const db = require("../mongo/schema");
 const Item = db.Item
+const User = db.User
 
 const Query = {
   items: async () => {
@@ -9,6 +10,11 @@ const Query = {
   item: async (obj, args, context, info) => {
     const item = await Item.findById(args.id); //with id
     return item;
+  },
+  me: async (obj, args, context, info) => {
+    const user = await User.findById(context.request.userId)
+    if (!user) return null
+    return user
   }
 };
 
