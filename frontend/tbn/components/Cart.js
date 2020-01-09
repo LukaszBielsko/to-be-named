05d6ab00da12e3f10617f32d65ff8210395cf46e
Lukasz Walczak <lukeabove@gmail.com>
2020-01-09 13:29:58 +0100

    Cart component added

---
+++ frontend/tbn/components/Cart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CartStyles from './styles/cartStyles';
+
+const Cart = props => (
+  <CartStyles open={false}>
+    <p>this is your cart, man</p>
+  </CartStyles>
+);
+
+export default Cart;
