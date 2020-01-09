dcbd173e39bbcf6607be00133c26b4731966f3c9
Lukasz Walczak <lukeabove@gmail.com>
2020-01-09 11:39:01 +0100

    product component added

--- frontend/tbn/pages/test1.js
+++ frontend/tbn/pages/test1.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PleaseSignIn from '../components/PleaseSignIn';
+import Product from '../components/Product';
 
 const test1 = props => (
-  <PleaseSignIn message="This is test message :) if you are not logged in">
-    <p>you are logged in, mate</p>
-  </PleaseSignIn>
+  <>
+    <PleaseSignIn message="This is test message :) if you are not logged in">
+      <p>you are logged in, mate</p>
+    </PleaseSignIn>
+    <Product />
+  </>
 );
 
 export default test1;
