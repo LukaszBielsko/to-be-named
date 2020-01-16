973bbcba2f1cd2383023d51bdd4433f63dcede70
Lukasz Walczak <lukeabove@gmail.com>
2020-01-16 11:13:27 +0100

    all orders grapQL query / mongo resolver added

--- backend/resolvers/Query.js
+++ backend/resolvers/Query.js
@@ -28,6 +28,13 @@ const Query = {
     const order = await user.orders.id(id);
     return order;
   },
+  allOrders: async (obj, args, context, info) => {
+    // if (!context.request.userId) return null;
+    // const user = await User.findById(context.request.userId);
+    const user = await User.findById('5e1835fd1ef8621ccd64ae70');
+    const orders = await user.orders;
+    return orders;
+  },
 };
 
 module.exports = Query;
