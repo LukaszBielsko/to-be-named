67c527f3df0c0c98424a85481d613f721ca1838c
Lukasz Walczak <lukeabove@gmail.com>
2020-01-16 10:04:36 +0100

    styles added to order component

--- frontend/tbn/components/Order.js
+++ frontend/tbn/components/Order.js
@@ -1,14 +1,36 @@
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
+import { formatMoney } from '../lib/utils';
 
 const ORDER_QUERY = gql`
   query ORDER_QUERY($id: String!) {
     order(id: $id) {
       total
+      _id
       products {
         title
         price
+        _id
       }
+    }
+  }
+`;
+
+const StyledOrder = styled.div`
+  margin: auto;
+  width: auto;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  padding: 30px;
+  background-color: white;
+  width: 50%;
+  margin-top: 30px;
+  table {
+    thead {
+      font-size: 2.7rem;
+    }
+    td {
+      width: 200px;
     }
   }
 `;
@@ -17,17 +39,29 @@ const Order = ({ orderId }) => (
   <Query query={ORDER_QUERY} variables={{ id: orderId }}>
     {({ data, loading }) => {
       if (loading) return <p>...loading order...</p>;
-      const { total, products } = data.order;
-      console.log(total);
+      const { total, products, _id } = data.order;
       return (
-        <>
-          <p>order total: {total}</p>
-          {products.map(product => (
-            <p>
-              product name: {product.title} product price: {product.price}
-            </p>
-          ))}
-        </>
+        <StyledOrder>
+          <h2>Thank you!</h2>
+          <h3>order number: {_id}</h3>
+          <h3>order total: $ {formatMoney(total)}</h3>
+          <table>
+            <thead>
+              <tr>
+                <td>Product</td>
+                <td>Price</td>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => (
+                <tr key={`${product._id}/${index}`}>
+                  <td>{product.title}</td>
+                  <td>$ {formatMoney(product.price)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </StyledOrder>
       );
     }}
   </Query>
