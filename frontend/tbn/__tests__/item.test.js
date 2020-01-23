5d1c69ba563c32db858d34963accafdc20ce6ec9
Lukasz Walczak <lukeabove@gmail.com>
2020-01-23 15:22:07 +0100

    item component tests added

---
+++ frontend/tbn/__tests__/item.test.js
@@ -0,0 +1,43 @@
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import ItemComponent from '../components/Item';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const fakeItem = {
+  _id: 'abc',
+  title: 'product',
+  place: 'china',
+  description: 'great product',
+  largeImage: 'large.jpg',
+};
+
+describe('<Item />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<ItemComponent {...fakeItem} />);
+  });
+
+  it('renders component properly', () => {
+    expect(wrapper.length).toBe(1);
+  });
+
+  it('displays proper place', () => {
+    const place = wrapper.find('.item-place');
+    expect(place.text()).toBe('china');
+  });
+
+  it('displays proper description', () => {
+    const description = wrapper.find('.item-description');
+    expect(description.text()).toBe('great product');
+  });
+
+  it('proper img src is passed', () => {
+    const img = wrapper.find('img');
+    expect(img.prop('src')).toEqual('large.jpg');
+  });
+
+  it('proper id is passed', () => {
+    /* TODO check for id */
+  });
+});
